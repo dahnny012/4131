9105cb6b4a8b3547ee9a40a9fa2a9f88bdf00bca
Danh Nguyen <filler012@gmail.com>
2015-03-23 17:21:30 +0000

    converted to tabs

--- 4211/client.py
+++ 4211/client.py
@@ -10,152 +10,152 @@ CONTENTS = 1
 COMMAND = 0
 
 class Token:
-    def createToken(self,data):
-            msg = data.split(" ")
-            if msg[EVENT] == "ACCEPTED":
-                return int(msg[CONTENTS])
-            else:
-                print("Did not get a token")
-                return -1
-            
+	def createToken(self,data):
+			msg = data.split(" ")
+			if msg[EVENT] == "ACCEPTED":
+				return int(msg[CONTENTS])
+			else:
+				print("Did not get a token")
+				return -1
+			
 factory = Token()
 
 class FtpClient:
-    def __init__(self):
-        self.socket = None
-        self.token = ""
-        self.quit = False
-        self.handlers = {}
-        self.handlers["ls"] = self.ls
-        self.handlers["download"] = self.download
-        self.handlers["upload"] = self.upload
-        self.handlers["quit"] = self.disconnect
-        self.request = ""
-        return
-    def run(self):
-        self.login()
-        self.process()
-        return
-    def handle(self):
-        try:
-            header = self.request.split(" ")
-            return self.handlers[header[COMMAND]](header)
-        except:
-            print("Command does not exist")
-            return False
-    def process(self):
-        while not self.quit:
-            validCommand = False
-            #   Attempt to get a valid request
-            while not validCommand:
-                self.request = input("Command: " )
-                validCommand = self.handle()
-    def login(self):
-        connected = False
-        while not connected:
-            self.connect()
-            print("Please login")
-            user = input('Enter your user: ')
-            password = input('Enter your pass: ')
-            
-            #Should encrypt but... w/e
-            self.send("login "+user + " " + password)
-            
-            #Recieve from server
-            data = self.recieve(1024).decode("ASCII")
-            
-            # See if you got a token
-            token = factory.createToken(data)
-            if token != -1:
-                self.token = token
-                connected = True
-            self.socket.close()
-            
-    def connect(self):
-        self.socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        try:
-            self.socket.connect((HOST, PORT))
-        except:
-            print("Error connecting")
-             
-          
-    def sign(self):
-        self.request = self.request +  "\n" + str(self.token) + "\n" + "\r"
-        # Default just sends command and prints output
-    def ls(self,header):
-        self.sign()
-        self.connect()
-        self.send(self.request)
-        content = self.recieveAll()
-        print(content)
-        self.close()
-        return True
-    def upload(self,header):
-        self.sign()
-        self.connect()
-        self.send(self.request)
-        try:
-            with open(header[CONTENTS],"rb") as file:
-                while True:
-                    buf = file.read(1024)
-                    if not buf:
-                        break
-                    self.sendB(buf)
-            self.close()
-            self.request = "ls"
-            self.ls(None)
-            return True
-        except:
-            print("error on upload")
-            return False
-    def download(self,header):
-        self.sign()
-        try:
-            self.connect()
-            self.send(self.request)
-            self.recFile(header[CONTENTS])
-            self.close()
-            return True
-        except Exception:
-            traceback.print_exc()
-            return False
-    def disconnect(self,header):
-        self.quit = True
-        self.sign()
-        return True
-    
-    def recFile(self,fn):
-        if(self.fileError()):
-            print("Server could not process your request")
-            return False
-        with open("rec.jpg","wb") as file:
-            while(True):
-                buf = self.recieve(1024)
-                if not buf:
-                    break
-                file.write(buf)
-    def fileError(self):
-        buf = self.recieve(64).decode("ASCII")
-        buf = buf.split(" ")
-        return buf[EVENT] == "ERROR"
-        
-    def close(self):
-        self.socket.close()
-        self.request = ""
-    def send(self,msg):
-        self.socket.send(bytes(msg, 'ASCII'))
-    def sendB(self,msg):
-        self.socket.send(msg)
-    def recieve(self,size):
-        return self.socket.recv(size)
-    def recieveAll(self):
-        data = ""
-        while(True):
-            buf = self.socket.recv(1024)
-            if not buf:
-                break
-            data += buf.decode('ASCII')
-        return data
-    
+	def __init__(self):
+		self.socket = None
+		self.token = ""
+		self.quit = False
+		self.handlers = {}
+		self.handlers["ls"] = self.ls
+		self.handlers["download"] = self.download
+		self.handlers["upload"] = self.upload
+		self.handlers["quit"] = self.disconnect
+		self.request = ""
+		return
+	def run(self):
+		self.login()
+		self.process()
+		return
+	def handle(self):
+		try:
+			header = self.request.split(" ")
+			return self.handlers[header[COMMAND]](header)
+		except:
+			print("Command does not exist")
+			return False
+	def process(self):
+		while not self.quit:
+			validCommand = False
+			#   Attempt to get a valid request
+			while not validCommand:
+				self.request = input("Command: " )
+				validCommand = self.handle()
+	def login(self):
+		connected = False
+		while not connected:
+			self.connect()
+			print("Please login")
+			user = input('Enter your user: ')
+			password = input('Enter your pass: ')
+			
+			#Should encrypt but... w/e
+			self.send("login "+user + " " + password)
+			
+			#Recieve from server
+			data = self.recieve(1024).decode("ASCII")
+			
+			# See if you got a token
+			token = factory.createToken(data)
+			if token != -1:
+				self.token = token
+				connected = True
+			self.socket.close()
+			
+	def connect(self):
+		self.socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+		try:
+			self.socket.connect((HOST, PORT))
+		except:
+			print("Error connecting")
+			 
+		  
+	def sign(self):
+		self.request = self.request +  "\n" + str(self.token) + "\n" + "\r"
+		# Default just sends command and prints output
+	def ls(self,header):
+		self.sign()
+		self.connect()
+		self.send(self.request)
+		content = self.recieveAll()
+		print(content)
+		self.close()
+		return True
+	def upload(self,header):
+		self.sign()
+		self.connect()
+		self.send(self.request)
+		try:
+			with open(header[CONTENTS],"rb") as file:
+				while True:
+					buf = file.read(1024)
+					if not buf:
+						break
+					self.sendB(buf)
+			self.close()
+			self.request = "ls"
+			self.ls(None)
+			return True
+		except:
+			print("error on upload")
+			return False
+	def download(self,header):
+		self.sign()
+		try:
+			self.connect()
+			self.send(self.request)
+			self.recFile(header[CONTENTS])
+			self.close()
+			return True
+		except Exception:
+			traceback.print_exc()
+			return False
+	def disconnect(self,header):
+		self.quit = True
+		self.sign()
+		return True
+	
+	def recFile(self,fn):
+		if(self.fileError()):
+			print("Server could not process your request")
+			return False
+		with open("rec.jpg","wb") as file:
+			while(True):
+				buf = self.recieve(1024)
+				if not buf:
+					break
+				file.write(buf)
+	def fileError(self):
+		buf = self.recieve(64).decode("ASCII")
+		buf = buf.split(" ")
+		return buf[EVENT] == "ERROR"
+		
+	def close(self):
+		self.socket.close()
+		self.request = ""
+	def send(self,msg):
+		self.socket.send(bytes(msg, 'ASCII'))
+	def sendB(self,msg):
+		self.socket.send(msg)
+	def recieve(self,size):
+		return self.socket.recv(size)
+	def recieveAll(self):
+		data = ""
+		while(True):
+			buf = self.socket.recv(1024)
+			if not buf:
+				break
+			data += buf.decode('ASCII')
+		return data
+	
 client = FtpClient()
 client.run()
--- 4211/server.py
+++ 4211/server.py
@@ -14,142 +14,142 @@ CONTENTS = 1
 EXCESS = 3
 
 class Tokens:
-    def __init__(self):
-        #Private but dunno how to.
-        self.tokens = {}
-    def createToken(self):
-        token = randint(1000,9999)
-        self.tokens[token] = token
-        return str(token)
-    def checkToken(self,token):
-        try:
-            if int(token) in self.tokens:
-                return True
-        except:
-            print("not a number")
-        return False
-    def killToken(self,token):
-        self.tokens[token] = None
+	def __init__(self):
+		#Private but dunno how to.
+		self.tokens = {}
+	def createToken(self):
+		token = randint(1000,9999)
+		self.tokens[token] = token
+		return str(token)
+	def checkToken(self,token):
+		try:
+			if int(token) in self.tokens:
+				return True
+		except:
+			print("not a number")
+		return False
+	def killToken(self,token):
+		self.tokens[token] = None
 
 
 class FtpServer:
-    def __init__(self):
-        self.users = {}
-        with open("records.txt","r") as file:
-            for line in file:
-                line = line.split(" ")
-                self.users[line[0]]=line[1].split("\n")[0]
-        self.socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.socket.bind((socket.gethostname(),PORT))
-        self.socket.listen(10)
-        print("Starting server " + socket.gethostname() + " at Port: " + PORT)
-        self.tokens = Tokens()
-        self.handles = {}
-        self.handles["login"] = self.login
-        self.handles["ls"] = self.ls
-        self.handles["upload"] = self.upload
-        self.handles["download"] = self.download
-        
-    def run(self):
-        while True:
-        	client_socket,address = self.socket.accept()
-        	t = threading.Thread(target=self.route, args=(client_socket,))
-        	t.run();
-        	
-    def route(self,client):
-        header = client.recv(64)
-        header = header.decode("utf-8")
-        args = header.split("\n")[0].split(" ")
-        print("Routing to " + args[COMMAND])
-        self.handles[args[COMMAND]](client,header)
-        return
-    
-    def login(self,client,header):
-        token = None
-        header = header.split(" ")
-        token = self.checkUserPass(header[USER],header[PASSWORD])
-        #print("No login credentials")
-        if token == None:
-                print("Rejected a user")
-                client.send(bytes("INVALID",'ASCII'))
-                client.close()
-                return
-        print("Accepted a user")
-        client.send(bytes("ACCEPTED " + str(token),'ASCII'))
-        client.close()
-        return
-    def ls(self,client,header):
-        if self.authToken(header):
-            print("command accepted")
-            files="file1.txt\nfile2.txt"
-            client.send(bytes(files,'ASCII'))
-        else:
-            print("token rejected")
-            client.send(bytes("ERROR No AuthToken",'ASCII'))
-        client.close()
-        
-    def upload(self,client,msg):
-        if self.authToken(msg):
-            print("command accepted")
-            print(msg)
-            headerEnd = msg.find("\r")
-            header = msg[0:headerEnd]
-            file = header.split("\n")[0].split(" ")[CONTENTS]
-            excess = bytes(msg[headerEnd+1:],'ASCII')
-            print(excess)
-            #should be not but well ignore for now.
-            if os.path.isfile(file):
-                with open("uploaded.jpg" , "wb") as file:
-                    file.write(excess)
-                    while True:
-                        buf = client.recv(1024)
-                        if not buf:
-                            break
-                        file.write(buf)
-            else:
-                print("File already exists")
-                client.send(bytes("ERROR File Exists",'ASCII'))
-        else:
-            print("token rejected")
-            client.send(bytes("ERROR No AuthToken",'ASCII'))
-        client.close()
-    def download(self,client,header):
-        if self.authToken(header):
-            print("command accepted")
-            args = header.split("\n")[0].split(" ")
-            try:
-                with open(args[CONTENTS] , "rb") as file:
-                    client.send(bytes("OK","ASCII"))
-                    while True:
-                        buf = file.read(1024)
-                        if not buf:
-                            break
-                        client.send(buf)
-            except:
-                traceback.print_exc()
-                client.send(bytes("ERROR","ASCII"))
-        else:
-            print("token rejected")
-            client.send(bytes("ERROR No AuthToken",'ASCII'))
-        client.close()
+	def __init__(self):
+		self.users = {}
+		with open("records.txt","r") as file:
+			for line in file:
+				line = line.split(" ")
+				self.users[line[0]]=line[1].split("\n")[0]
+		self.socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+		self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+		self.socket.bind((socket.gethostname(),PORT))
+		self.socket.listen(10)
+		print("Starting server " + socket.gethostname() + " at Port: " + PORT)
+		self.tokens = Tokens()
+		self.handles = {}
+		self.handles["login"] = self.login
+		self.handles["ls"] = self.ls
+		self.handles["upload"] = self.upload
+		self.handles["download"] = self.download
+		
+	def run(self):
+		while True:
+			client_socket,address = self.socket.accept()
+			t = threading.Thread(target=self.route, args=(client_socket,))
+			t.run();
+			
+	def route(self,client):
+		header = client.recv(64)
+		header = header.decode("utf-8")
+		args = header.split("\n")[0].split(" ")
+		print("Routing to " + args[COMMAND])
+		self.handles[args[COMMAND]](client,header)
+		return
+	
+	def login(self,client,header):
+		token = None
+		header = header.split(" ")
+		token = self.checkUserPass(header[USER],header[PASSWORD])
+		#print("No login credentials")
+		if token == None:
+				print("Rejected a user")
+				client.send(bytes("INVALID",'ASCII'))
+				client.close()
+				return
+		print("Accepted a user")
+		client.send(bytes("ACCEPTED " + str(token),'ASCII'))
+		client.close()
+		return
+	def ls(self,client,header):
+		if self.authToken(header):
+			print("command accepted")
+			files="file1.txt\nfile2.txt"
+			client.send(bytes(files,'ASCII'))
+		else:
+			print("token rejected")
+			client.send(bytes("ERROR No AuthToken",'ASCII'))
+		client.close()
+		
+	def upload(self,client,msg):
+		if self.authToken(msg):
+			print("command accepted")
+			print(msg)
+			headerEnd = msg.find("\r")
+			header = msg[0:headerEnd]
+			file = header.split("\n")[0].split(" ")[CONTENTS]
+			excess = bytes(msg[headerEnd+1:],'ASCII')
+			print(excess)
+			#should be not but well ignore for now.
+			if os.path.isfile(file):
+				with open("uploaded.jpg" , "wb") as file:
+					file.write(excess)
+					while True:
+						buf = client.recv(1024)
+						if not buf:
+							break
+						file.write(buf)
+			else:
+				print("File already exists")
+				client.send(bytes("ERROR File Exists",'ASCII'))
+		else:
+			print("token rejected")
+			client.send(bytes("ERROR No AuthToken",'ASCII'))
+		client.close()
+	def download(self,client,header):
+		if self.authToken(header):
+			print("command accepted")
+			args = header.split("\n")[0].split(" ")
+			try:
+				with open(args[CONTENTS] , "rb") as file:
+					client.send(bytes("OK","ASCII"))
+					while True:
+						buf = file.read(1024)
+						if not buf:
+							break
+						client.send(buf)
+			except:
+				traceback.print_exc()
+				client.send(bytes("ERROR","ASCII"))
+		else:
+			print("token rejected")
+			client.send(bytes("ERROR No AuthToken",'ASCII'))
+		client.close()
 
-    def checkUserPass(self,user,password):
-        print(user)
-        print(password)
-        if user in self.users:
-            if self.users[user] in password:
-                return self.tokens.createToken()
-            else:
-                print("Password did not match")
-        else:
-            print("user not in records")
-        return None
-        
-    def authToken(self,request):
-        args = request.split("\n")
-        token = args[TOKEN]
-        return self.tokens.checkToken(token)
+	def checkUserPass(self,user,password):
+		print(user)
+		print(password)
+		if user in self.users:
+			if self.users[user] in password:
+				return self.tokens.createToken()
+			else:
+				print("Password did not match")
+		else:
+			print("user not in records")
+		return None
+		
+	def authToken(self,request):
+		args = request.split("\n")
+		token = args[TOKEN]
+		return self.tokens.checkToken(token)
 
 server = FtpServer()
 server.run()
